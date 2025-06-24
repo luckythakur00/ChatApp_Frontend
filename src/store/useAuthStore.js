aedce134143ea111a0bd63a81b770752ec929e10
Lucky <luckythakur13709@gmail.com>
2025-06-24 18:16:54 +0530

    Added Backend URL

--- src/store/useAuthStore.js
+++ src/store/useAuthStore.js
@@ -3,7 +3,7 @@ import { axiosInstance } from '../lib/axios';
 import { toast } from 'react-toastify';
 import { io } from "socket.io-client";
 
-const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:3000" : "/";
+const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:3000" : "https://chatappbackend-production-cd45.up.railway.app";
 
 export const useAuthStore = create((set, get) => ({
     authUser: null,
