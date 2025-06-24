ecea03fcb57798f744fe297449ce60111fe899f0
Lucky <luckythakur13709@gmail.com>
2025-06-24 18:09:32 +0530

    Updated backend link

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 export const axiosInstance = axios.create({
-    baseURL: import.meta.env.MODE === "development" ? "http://localhost:3000/api" : "/api",
+    // baseURL: import.meta.env.MODE === "development" ? "http://localhost:3000/api" : "/api",
+    baseURL: import.meta.env.MODE === "development" ? "http://localhost:3000/api" : "https://chatappbackend-production-cd45.up.railway.app/api",
     withCredentials: true,
 })
